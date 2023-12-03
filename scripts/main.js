2d17888a4d33e663e569699d119e969e065dbaae
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2023-12-03 11:43:53 +0200

    change 4

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
-import { getCountries } from "./services/countries.service";
+import { getCountries } from "./services/countries.service.js";
+import { createCardsList } from "./services/dom.service.js";
 
 console.log(await getCountries());
+
+createCardsList();
+
 
 // let obj = {
 //     1: 'a',
@@ -9,3 +13,9 @@ console.log(await getCountries());
 // let j = JSON.stringify(obj)
 // console.log(obj);
 // console.log(JSON.parse(j));
+
+// const p = document.createElement('p');
+// p.textContent = 'blabla';
+// p.className = 'text-light';
+
+// document.body.appendChild(p);
--- scripts/services/dom.service.js
+++ scripts/services/dom.service.js
@@ -1,7 +1,49 @@
+import { getCountries } from "./countries.service.js";
+
 const searchInput = document.getElementById("search");
 const cards = document.getElementById("cards");
 
-const createCard = (card) => {
+const createCard = (country) => {
     const card = document.createElement("div");
     card.className = 'card';
+
+    const cardImg = document.createElement("img");
+    cardImg.className = 'card-img-top img border rounded shadow';
+    cardImg.src = country.flags.png;
+
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+
+    const cardTitle = document.createElement('h3');
+    cardTitle.className = 'card-title';
+    cardTitle.textContent = country.name.common;
+
+    const population = document.createElement('p');
+    population.className = 'card-text';
+    population.textContent = `population: ${country.population}`;
+
+    const cardFooter = document.createElement('div');
+    cardFooter.className = 'card-footer';
+
+    const heart = document.createElement('i');
+    heart.className = 'fa fa-heart';
+
+    card.appendChild(cardImg);
+
+
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(population);
+
+    card.appendChild(cardBody);
+
+    cardFooter.appendChild(heart);
+    card.appendChild(cardFooter);
+
+    cards.appendChild(card);
+}
+
+export const createCardsList = async () => {
+    for (const item of await getCountries()) {
+        createCard(item);
+    }
 }
